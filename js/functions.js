9e70306358b361806361cd9e712fd73fcdff8c24
Antoine Bollinger <antoine.bollinger@gmail.com>
2020-12-29 16:42:23 +0100

    new function Cart

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,5 @@
 // Récupération variable GET dans URL
+const currentUrl = window.location.pathname.split("/").pop();
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const myId = (urlParams.get('myId') === null) ? "" : urlParams.get("myId");
@@ -9,6 +10,9 @@ const mainDiv = document.querySelector("main");
 
 // API
 const apiUrl = (myUrl == "hero") ? "https://oc-p5-api.herokuapp.com/api/teddies/" : "http://localhost:3000/api/teddies/" ;
+
+
+
 
 
 // FONCTIONS AFFICHAGE 1 PRODUIT POUR PAGE PRODUIT
@@ -23,25 +27,28 @@ function displayOneCard(array, pend = "") {
       mainDiv.appendChild(newOneCard);
       break;
   }
-  // Div image
-  const newImgDiv = createEle("div", "", ["col-lg-7"],"", "", newOneCard);
-  createEle("img", "", ["img-fluid", "rounded", "mb-4", "mb-lg-0"], {"src": array.imageUrl}, "", newImgDiv);
-  // Div info
-  const newInfoDiv = createEle("div", "", ["col-lg-5"], "", "", newOneCard);
-  // Titre avec prix
-  createEle("h1", "", "", "", "<span>"+array.name+"</span><span>"+(array.price / 100)+" €<span>", newInfoDiv);
-  // Description 
-  createEle("p", "", "", "", array.description, newInfoDiv);
-  // Label couleur
-  const newLabel = createEle("label", "", "", {"for": "mainSelect"}, "Couleurs disponibles : ", newInfoDiv);
-  const newSelect = createEle("select", "mainSelect", "", "", "", newLabel);
-  for (let ele of array.colors) {
-    createEle("option", "", "", "", ele, newSelect);
-  }
-  // Bouton ajouter
-  createEle("a", "buttonAddToCart", ["btn", "btn-primary"], {"data-id": array._id}, "<i class=\"fas fa-cart-plus\"></i> Ajouter au panier", newInfoDiv);
+    // Div image
+    const newImgDiv = createEle("div", "", ["col-lg-7"],"", "", newOneCard);
+      createEle("img", "", ["img-fluid", "rounded", "mb-4", "mb-lg-0"], {"src": array.imageUrl}, "", newImgDiv);
+    // Div info
+    const newInfoDiv = createEle("div", "", ["col-lg-5"], "", "", newOneCard);
+      // Titre avec prix
+      createEle("h1", "", "", "", "<span>"+array.name+"</span><span>"+displayPrice(array.price / 100)+"<span>", newInfoDiv);
+      // Description 
+      createEle("p", "", "", "", array.description, newInfoDiv);
+    // Label couleur
+    const newLabelColor = createEle("label", "", "", {"for": "selectColor"}, "Couleurs disponibles : ", newInfoDiv);
+      const newSelectColor = createEle("select", "selectColor", "", {"name": "selectColor"}, "", newLabelColor);
+        for (let ele of array.colors) {
+          createEle("option", "", "", "", ele, newSelectColor);
+        }
+    // Label quantité
+    const newLabelQty = createEle("label", "", "", {"for": "inputQty"}, "Quantité : ",newInfoDiv);
+      const newInputQty = createEle("input", "inputQty", "", {"type": "number", "name": "inputQty", "min": 1, "value": 1}, "", newLabelQty);
+      // Bouton ajouter
+        createEle("a", "addToCart", ["btn", "btn-primary"], {"data-id": array._id, "data-action": "addToCart"}, "<i class=\"fas fa-cart-plus\"></i> Ajouter au panier", newInfoDiv);
 }
-
+// APPEL DE LA FONCTION PRECEDENTE AVEC LES DONNEES JSON
 async function getOneProduct(pend) {
   await fetch(apiUrl+myId)
     .then((response) => response.json())
@@ -57,22 +64,22 @@ function displayAllCard(array, nb = 3) {
     if (ele._id != myId && nbTmp < nb) {
       // Div princpal carte
       const newArticle = createEle("div", "", ["col-lg-4", "col-md-6", "mb-4"], "", "", mainProducts);
-      // Div secondaire carte
-      const newCard = createEle("div", "", ["card", "h-100"], "", "", newArticle);
-      // Div image
-      createEle("img", "", ["card-img-top"], {"src": ele.imageUrl, "alt": ele.name}, "", newCard);
-      // Div infos
-      const newInfoDiv = createEle("div", "", ["card-body"], "", "", newCard);
-      createEle("h2", "", ["card-title"], "", ele.name, newInfoDiv);
-      createEle("p", "", ["card-text"], "", ele.description, newInfoDiv);
-      // Div Footer
-      const newFooter = createEle("div", "", ["card-footer"], "", "", newCard);
-      createEle("a", ele._id, ["btn", "btn-primary"], {"href": "product.html?myId="+ele._id}, "<i class=\"far fa-eye\"></i> Découvrir !", newFooter);
-      nbTmp++;
+        // Div secondaire carte
+        const newCard = createEle("div", "", ["card", "h-100"], "", "", newArticle);
+          // Div image
+          createEle("img", "", ["card-img-top"], {"src": ele.imageUrl, "alt": ele.name}, "", newCard);
+          // Div infos
+          const newInfoDiv = createEle("div", "", ["card-body"], "", "", newCard);
+            createEle("h2", "", ["card-title"], "", ele.name, newInfoDiv);
+            createEle("p", "", ["card-text"], "", ele.description, newInfoDiv);
+          // Div Footer
+          const newFooter = createEle("div", "", ["card-footer"], "", "", newCard);
+            createEle("a", ele._id, ["btn", "btn-primary"], {"href": "product.html?myId="+ele._id}, "<i class=\"far fa-eye\"></i> Découvrir !", newFooter);
+          nbTmp++;
     }
   }
 }
-
+// APPEL DE LA FONCTION PRECEDENTE AVEC LES DONNEES JSON
 async function getAllProducts(nb="") {
   await fetch(apiUrl)
     .then((response) => response.json())
@@ -110,16 +117,11 @@ function createEle(_type = "div", _id = "", _class = [], _attribute = [], _conte
   if (typeof(_parent) != 'undefined' && _parent != null) {
     _parent.appendChild(e);
   }
-    return e;
+  return e;
 }
 
-// FONCTION AFFICHAGE NB PRODUITS DANS PANIER DANS NAV
-function updateNbrPanier() {
-  const qtePanier = document.getElementById("qtePanier");
-  if (window.localStorage.length == 0) {
-      qtePanier.style.visibility = "hidden";
-  } else {
-      qtePanier.style.visibility = "visible";
-      qtePanier.innerHTML = window.localStorage.length;
-  }
+// FONCTION AFFICHAGE PRIX FRANCE
+function displayPrice(_price) {
+  const newPrice = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(_price);
+  return newPrice;
 }
--- js/functionsCart.js
+++ js/functionsCart.js
@@ -1,57 +1,185 @@
-// --------------------- FONCTIONS LOCALSTORAGE ----------------------------//
-
-// LOCALSTORAGE INIT
-updateNbrPanier();
-
-if (typeof(Storage) !== "undefined") {
-  updateNbrPanier();
-} else {
-  console.log("error");
-}
-
 // SWITCH ACTION
 document.addEventListener("click", function(e) {
   if (e.target) {
-    switch (e.target.id) {
-      case "buttonAddToCart": 
+    switch (e.target.dataset.action) {
+      case "addToCart": 
         e.preventDefault();
-        console.log("add to cart"+buttonAddToCart.dataset.id);
-        addProdToCart(buttonAddToCart.dataset.id);
+        const myColor = document.getElementById("selectColor");
+        const myQty = document.getElementById("inputQty");
+        addToCart(e.target.dataset.id, parseInt(myQty.value), myColor.value);
+        break;
+      case "refreshCart":
+        e.preventDefault();
+        const myIndex = e.target.dataset.index;
+        const myNewQty = document.getElementById("quantite_"+myIndex);
+        refreshCart(myIndex, myNewQty.value);
+        console.log(myNewQty.value);
+        break;
+      case "removeFromCart": 
+        e.preventDefault();
+        removeFromCart(e.target.dataset.index);
         break;
       case "clearCart":
-        if (sizeCart() != 0) {
-          if (confirm("Vous êtes certain de vouloir vider le panier ?")) {
-            e.preventDefault();
-            clearCart()
-            updateNbrPanier();
-          }
-        }
+        e.preventDefault();
+        clearCart();
         break;
     }
   }
 });
 
+const cartDiv = document.getElementById("cartDiv");
+
+updateQtyCart();
+displayCart();
+
+
+// --------------------- AFFICHAGE PANIER --------------------------------//
+
+function displayCart() {
+  if (currentUrl == "panier.html") {
+    updateQtyCart();
+    cartDiv.innerHTML = "";
+    const myCart = JSON.parse(window.localStorage.getItem("products"));
+    for (let ele of myCart) {
+      displayProduct(ele, myCart.indexOf(ele));
+    }
+    createEle("div", "", "", "", JSON.stringify(myCart), cartDiv);
+  }
+}
+
+async function displayProduct(array, index) {
+  await fetch(apiUrl+array.id)
+    .then((response) => response.json())
+    .then((nounours) => createItem(nounours, index, array.color, array.quantity))
+}
+
+function createItem(array, _index, _color, _quantity) {
+  const newProduct = createEle("div", "", ["row", "cart-product"], "", "", cartDiv);
+  //Image
+    const newDivImg = createEle("div", "", ["col", "col-sm", "col-md", "text-center"], "", "", newProduct);
+      createEle("img", "", ["img-responsive"], {"src": array.imageUrl, "alt": "preview", "width": 120, "height": 80}, "", newDivImg);
+    // Product Infos 
+    const newDivInfo = createEle("div", "", ["col", "text-sm-center", "col-sm", "text-md-left", "col-md"], "", "", newProduct);
+      createEle("h2", "", ["product-name"], "", array.name, newDivInfo);
+      createEle("p", "", "", "", "Couleur : "+_color, newDivInfo);
+    // Prix / Quantité, Supprimer
+    const newProductSub = createEle("div", "", ["col", "col-sm", "text-sm-center", "col-md", "text-md-right", "row"], "", "", newProduct);
+    // Prix
+      const newDivPrix = createEle("div", "", ["col", "col-sm", "col-md", "text-md-right"], {"style": "padding-top:5px"}, "", newProductSub);
+        createEle("h2", "", "", "", displayPrice(array.price / 100)+"&nbsp;<span class=\"text-muted\">x</span>&nbsp;", newDivPrix);
+      // Quantité
+      const newDivQte = createEle("div", "", ["col", "col-sm", "col-md"], "", "", newProductSub);
+        const newDivQteSub = createEle("div", "", ["quantity"], "", "", newDivQte);
+          createEle("input", "quantite_"+_index, ["qty"], {"type": "number", "value": _quantity, "step": 1, "min": 1, "title": "Qty", "size": 4}, "", newDivQteSub);
+      const newDivActualiser = createEle("div", "", ["col", "col-sm", "col-md", "text-right"], "", "", newProductSub);
+        createEle("a", "", ["btn", "btn-primary", "btn-xs", "a-refresh"], {"type": "button", "data-index": _index, "data-action": "refreshCart"}, "<i class=\"fas fa-sync-alt\"></i>", newDivActualiser);          
+      const newDivSupprimer = createEle("div", "", ["col", "col-sm", "col-md", "text-right"], "", "", newProductSub);
+        createEle("button", "", ["btn", "btn-outline-danger", "btn-xs"], {"type": "button", "data-index": _index, "data-action": "removeFromCart"}, "<i class=\"fa fa-trash\"></i>", newDivSupprimer);
+}
+
+
+// --------------------- FONCTIONS DE BASE PANIER -------------------------//
+
 // FONCTION AJOUT PRODUIT
-function addProdToCart(id) {
-  window.localStorage.setItem("newProduct",id);
-  updateNbrPanier();
-  alert("Votre article a bien été ajouté au panier.");
+function addToCart(_id, _qty, _color) {
+  let myCart = JSON.parse(window.localStorage.getItem("products"));
+  if (checkIsInCart(myCart, _id, _qty, _color)) {
+    const newProduct = {"id": _id, "color": _color, "quantity" : _qty};
+    myCart.push(newProduct);
+    window.localStorage.removeItem("products");
+    window.localStorage.setItem("products", JSON.stringify(myCart));
+    updateQtyCart();
+    alert("Votre article a bien été ajouté au panier.");
+  }
+}
+
+// FONCTION MISE A JOUR PANIER 
+function refreshCart(_index, _quantity) {
+  let myCart = JSON.parse(window.localStorage.getItem("products"));
+  for (let ele of myCart) {
+    if (myCart.indexOf(ele) == _index) {
+      ele.quantity = parseInt(_quantity);
+    }
+  }
+  window.localStorage.removeItem("products");
+  window.localStorage.setItem("products", JSON.stringify(myCart));
+  updateQtyCart();
+  displayCart();
+}
+
+// FONCTION SUPPRIMER PRODUIT
+function removeFromCart(_index) {
+  let myCart = JSON.parse(window.localStorage.getItem("products"));
+  if (confirm("Êtes-vous certain de vouloir supprimer ce produit de votre panier ?")) {
+    if (_index > -1) {
+      myCart.splice(_index, 1);
+    }
+    window.localStorage.removeItem("products");
+    window.localStorage.setItem("products", JSON.stringify(myCart));
+    updateQtyCart();
+    displayCart();
+  }
 }
 
 // FONCTION VIDER PANIER
 function clearCart() {
-  window.localStorage.clear();
-  updateNbrPanier();
+  if (sizeCart() != 0) {
+    if (confirm("Êtes-vous certain de vouloir vider le panier ?")) {
+      window.localStorage.clear();
+      updateQtyCart();
+      displayCart();
+    }
+  }
 }
 
 // FONCTION TAILLE PANIER
 function sizeCart() {
-  return window.localStorage.length;
+  const myCart = JSON.parse(window.localStorage.getItem("products"));
+  return myCart.length;
 }
 
 // FONCTION VERIFIER SI ELEMENT DEJA DANS PANIER
-function checkIsInCart(array, value) {
+function checkIsInCart(_cart, _id, _qty, _color) {
+  let valid = true;
+  for (let ele of _cart) {
+    if (ele.id == _id && ele.color == _color) {
+      if (confirm("Cet article est déjà présent dans votre panier. Souhaitez-vous augmenter la quantité ?")) {
+        ele.quantity += parseInt(_qty);
+        window.localStorage.removeItem("products");
+        window.localStorage.setItem("products", JSON.stringify(_cart)); 
+        updateQtyCart();
+        displayCart();
+      }
+      valid = false;
+    }
+  }
+  return valid;
+}
 
 
-  
+// --------------------- FONCTIONS INITIALISATION LOCALSTORAGE ----------------------------//
+
+// LOCALSTORAGE INIT ET UPDATE AFFICHAGE ICONE PANIER
+function updateQtyCart() {
+  if (typeof(Storage) !== "undefined") { 
+    // Initialise le panier en tant qu'item du localStorage
+    if (window.localStorage.getItem("products") === null) {
+      window.localStorage.setItem("products",JSON.stringify([]));
+    }
+    const displayQtyCart = document.getElementById("qtePanier");
+    const myCart = JSON.parse(window.localStorage.getItem("products"));
+    // Met à jour le chiffre sur l'icone panier du menu
+    if (myCart.length == 0) {
+      displayQtyCart.style.visibility = "hidden";
+    } else {
+      displayQtyCart.style.visibility = "visible";
+      nbr = 0;
+      for (let ele of myCart) {
+        nbr += parseInt(ele.quantity);
+      }
+      displayQtyCart.innerHTML = nbr;
+    }
+  } else {
+    console.log("error");
+  }
 }
